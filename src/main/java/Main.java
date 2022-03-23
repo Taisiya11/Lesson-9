3f9fffe6fa32c8be4a83ae976fc74167f044ab5b
tasya <taisiya_struchaeva@mail.ru>
2022-03-23 14:40:30 +0300

    Exercise_2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,8 +4,14 @@ public class Main {
     public static void main(String[] args) {
         ReadsFromFile readsFromFile = new ReadsFromFile();
         readsFromFile.readsFromFile("C:\\Users\\admin\\IdeaProjects\\Lesson6\\src\\main\\java\\fileForRead.txt");
-        System.out.println(readsFromFile.city);
-        Collections.sort(readsFromFile.city);
-        System.out.println(readsFromFile.city);
+        CacheSortList cacheSortList = new CacheSortList();
+        System.out.println(cacheSortList.parsesCacheData(readsFromFile.city));
+        System.out.println(cacheSortList.parsesCacheData(readsFromFile.city));
+
+
+
+//        System.out.println(readsFromFile.city);
+//        Collections.sort(readsFromFile.city);
+//        System.out.println(readsFromFile.city);
     }
 }
